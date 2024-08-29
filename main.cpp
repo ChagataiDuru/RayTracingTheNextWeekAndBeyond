1c772d4beab14ce529160ccf3f4b82408dd232eb
Çağatay <durucagatay01@gmail.com>
2024-08-29 23:22:12 +0300

    implementation of time & Motion blur

--- main.cpp
+++ main.cpp
@@ -45,20 +45,56 @@ int main(int argc, char* argv[]) {
 
     hittable_list world;
 
-    auto obj_material = make_shared<lambertian>(color(0.8, 0.4, 0.3));
-    auto obj_mesh = OBJLoader::load_obj("teapot.obj", obj_material);
-    for (const auto& triangle : obj_mesh) {
-        world.add(triangle);
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = RandomGenerator::instance().random_double();
+            point3 center(a + 0.9 * RandomGenerator::instance().random_double(), 0.2, b + 0.9 * RandomGenerator::instance().random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    auto center2 = center + vec3(0, RandomGenerator::instance().random_double(0, .5), 0);
+                    world.add(make_shared<sphere>(center, center2, 0.2, sphere_material));
+                }
+                else if (choose_mat < 0.95) {
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = RandomGenerator::instance().random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+                else {
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
     }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
     camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = image_width;
-    cam.samples_per_pixel = 1;
+    cam.samples_per_pixel = 10;
     cam.max_depth = 50;
 
     cam.vfov = 105;
-    cam.lookfrom = point3(5, 2, 3);
+    cam.lookfrom = point3(13, 2, 3);
     cam.lookat = point3(0, 0, 0);
     cam.vup = vec3(0, 1, 0);
 
--- ray.h
+++ ray.h
@@ -7,18 +7,25 @@ class ray {
 public:
     ray() {}
 
-    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+    ray(const point3& origin, const vec3& direction, double time)
+        : orig(origin), dir(direction), tm(time) {}
+
+    ray(const point3& origin, const vec3& direction)
+        : ray(origin, direction, 0) {}
 
     const point3& origin() const { return orig; }
     const vec3& direction() const { return dir; }
 
+    double time() const { return tm; }
+
     point3 rayPos(double t) const {
-        return point3(orig + t * dir);
+        return orig + t * dir;
     }
 
 private:
     point3 orig;
     vec3 dir;
+    double tm;
 };
 
 #endif
